d2dc1603e887864d5af24f4c4d73b5d0ca14115c
Vano <vano50@mail.ru>
2024-07-04 11:20:41 +0300

    Initial commit

---
+++ home-2-7-1/home-2-7-1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#define MODE 1
+#if MODE == 1
+int add(int a, int b) {
+	int c;
+	c = a + b;
+	return c;
+}
+#endif
+
+#ifndef MODE
+#error MODE укажите
+#endif
+
+int main(int argc, char** argv)
+{
+
+	setlocale(LC_ALL, "Russian");
+
+#if MODE == 0
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	int a = 0;
+	int b = 0;
+	std::cout << "Введите число 1:" << std::endl;
+	std::cin >> a;
+	std::cout << "Введите число 2:" << std::endl;
+	std::cin >> b;
+	std::cout << "Результат сложения:" << add(a, b);
+#else
+	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+
+	return 0;
+
+}
